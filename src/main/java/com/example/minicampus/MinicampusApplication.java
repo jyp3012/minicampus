e2795a8883ec7ac38104d3994bf35d375ad53ed7
pjy3012 <ppp0868@naver.com>
2022-07-15 15:19:22 +0900

    최초 등록!

---
+++ src/main/java/com/example/minicampus/MinicampusApplication.java
@@ -0,0 +1,13 @@
+package com.example.minicampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinicampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinicampusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/minicampus/MinicampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minicampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinicampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
